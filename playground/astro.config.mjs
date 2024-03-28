6597b1199ddb791aa66f5508ef97c6ce5c96caa8
Adam Matthiesen <amatthiesen@outlook.com>
2024-03-28 09:53:15 -0700

    init

---
+++ playground/astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from "astro/config";
+import astroStudioCMS from "@astrolicious/studiocms";
+import db from '@astrojs/db';
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://ikc4k48.astro-studiocms.xyz',
+	output: "server",
+	adapter: node({ mode: "standalone" }),
+	integrations: [
+		db(),
+		astroStudioCMS({
+			dbStartPage: false,
+			verbose: true,
+		}),
+	],
+});
